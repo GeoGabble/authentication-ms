afbccf9e082082b9d6520766167f7a5f7950c55d
Lakshya0257 <lakshyabhati24@gmail.com>
2024-05-16 22:14:45 +0530

    combined service

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.GRPC,
-    options: {
-      package: 'authentication',
-      protoPath: join(__dirname, './proto/authentication.proto'),
-      url: 'localhost:8088',
-  }});
-  await app.listen();
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+      transport: Transport.GRPC,
+      options: {
+        package: 'authentication',
+        protoPath: join(__dirname, './proto/authentication.proto'),
+        url: 'localhost:8088',
+    }});
+    
+  await app.startAllMicroservices();
+  await app.listen(3000);
 }
 bootstrap();
