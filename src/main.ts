3242f54be234c2e65752206ec9fa2a6fedd10494
Lakshya0257 <lakshyabhati24@gmail.com>
2024-05-16 23:33:40 +0530

    msg

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
       options: {
         package: 'authentication',
         protoPath: join(__dirname, './proto/authentication.proto'),
-        url: 'https://authentication-ms.onrender.com:8088',
+        url: 'authentication-ms.onrender.com:8088',
     }});
     
   await app.startAllMicroservices();
