ac33ec71f59b0f82e9502ca6c40742c1f6323ce6
Ubuntu <ubuntu@ip-172-31-36-248.ap-south-1.compute.internal>
2024-06-03 06:02:34 +0000

    interpreter setup

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'authentication-ms',
+      script: 'npm',
+      args: 'start',
+      interpreter: 'node',
+      // other configuration options...
+    },
+  ],
+};
